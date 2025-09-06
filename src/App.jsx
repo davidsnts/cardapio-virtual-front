253b52285141e5aab4872abb346de28f09ad6ae9
davidsnts <david.santos@delage.com.br>
2025-09-06 13:49:46 -0300

    Feat: create cart context

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Navbar from './components/Navbar';
 import ProdutoSelecionado from './pages/produtoSelecionado'
+import { CartProvider } from './context/CartContext';
 
 function App() {
 
   return (
-    <>
+    <CartProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/produtoSelecionado/:id" element={<ProdutoSelecionado />} />
       </Routes>
-    </>
+    </CartProvider>
   )
 }
 
